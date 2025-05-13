b74433bb3761e492f67c5eb296c30cf1070d150c
Blake Gigiolio <blake.gigiolio@mail.utoronto.ca>
2025-05-13 13:54:22 -0400

    disabled axios

--- ChrisCrumbs/src/App.jsx
+++ ChrisCrumbs/src/App.jsx
@@ -5,11 +5,11 @@ import gif from './assets/IMG_3205.gif'
 
 function App() {
   const [count, setCount] = useState(0)
-  const baseURL = 'http://localhost:5000'
+  // const baseURL = 'http://localhost:5000'
   let letters = [{url: "https://ia601205.us.archive.org/31/items/newsletter-dia-de-la-tierra-elegante-verde/Newsletter%20d%C3%ADa%20de%20la%20tierra%20elegante%20verde.pdf", date: "April 2025"}]
-  useEffect(() => {
-    axios.get(baseURL + "/")
-  })
+  // useEffect(() => {
+  //   axios.get(baseURL + "/")
+  // })
   return (
     <>
       <div id="main">
