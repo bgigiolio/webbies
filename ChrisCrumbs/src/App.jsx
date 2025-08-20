2de801aff5cfda2b804e64392e5d3710e46f0111
Blake Gigiolio <blake.gigiolio@mail.utoronto.ca>
2025-08-19 22:04:38 -0400

    July newsletter

--- ChrisCrumbs/src/App.jsx
+++ ChrisCrumbs/src/App.jsx
@@ -9,7 +9,8 @@ function App() {
   let letters = [
     {url: "https://ia601205.us.archive.org/31/items/newsletter-dia-de-la-tierra-elegante-verde/Newsletter%20d%C3%ADa%20de%20la%20tierra%20elegante%20verde.pdf", date: "April 2025"},
     {url: "https://ia600903.us.archive.org/28/items/green-minimalist-company-newsletter_202506/Green%20Minimalist%20Company%20Newsletter.pdf", date: "May 2025"},
-    {url: "https://ia600901.us.archive.org/14/items/jujfadu/jujfadu.pdf", date: "June 2025"}
+    {url: "https://ia600901.us.archive.org/14/items/jujfadu/jujfadu.pdf", date: "June 2025"},
+    {url: "https://ia600905.us.archive.org/6/items/chrisletter-july/ChrisletterJuly.pdf", date: "July 2025"}
   ]
   // useEffect(() => {
   //   axios.get(baseURL + "/")
