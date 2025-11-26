9142685fd6b554ba827d75f5defd2914713e334d
Blake Gigiolio <blake.gigiolio@mail.utoronto.ca>
2025-11-25 21:03:46 -0500

    October update

--- ChrisCrumbs/src/App.jsx
+++ ChrisCrumbs/src/App.jsx
@@ -12,7 +12,8 @@ function App() {
     {url: "https://ia600901.us.archive.org/14/items/jujfadu/jujfadu.pdf", date: "June 2025"},
     {url: "https://ia600905.us.archive.org/6/items/chrisletter-july/ChrisletterJuly.pdf", date: "July 2025"},
     {url: "https://ia601002.us.archive.org/2/items/guppy-test-5/guppy%20test%205.pdf", date: "August 2025"},
-    {url: "https://ia600205.us.archive.org/28/items/papageegee/papageegee.pdf", date: "September 2025"}
+    {url: "https://ia600205.us.archive.org/28/items/papageegee/papageegee.pdf", date: "September 2025"},
+    {url: "https://ia803404.us.archive.org/34/items/october-newsletter/October%20Newsletter.pdf", date: "October 2025"}
   ]
   // useEffect(() => {
   //   axios.get(baseURL + "/")
